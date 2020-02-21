f92e5d7dd37fe137218999ba8c0352ea27c834aa
Jack Sankey <jack.sankey@gmail.com>
2020-02-21 09:36:24 -0500

    Update playground.py

--- playground.py
+++ playground.py
@@ -1,3 +1,19 @@
+ # This file is part of the Macrospyn distribution 
+ # (https://github.com/Spinmob/macrospyn).
+ # Copyright (c) 2002-2020 Jack Childress (Sankey).
+ # 
+ # This program is free software: you can redistribute it and/or modify  
+ # it under the terms of the GNU General Public License as published by  
+ # the Free Software Foundation, version 3.
+ # 
+ # This program is distributed in the hope that it will be useful, but 
+ # WITHOUT ANY WARRANTY; without even the implied warranty of 
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ # General Public License for more details.
+ # 
+ # You should have received a copy of the GNU General Public License 
+ # along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import numpy       as _n
 import scipy.stats as _stats
 
